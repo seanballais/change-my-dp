e62dfb3afb15cefe68d6263c0fa66889dd948aa4
Sean Francis N. Ballais <sean@seanballais.com>
2018-04-19 23:33:53 +0800

    Things work, I guess. More stuff to do.

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import './App.css';
 
 function updateCanvasRGB(new_rgb) {
   this.setState({
-    rgb: new_rgb
+    rgb: new_rgb,
+    rotation: this.state.rotation
   });
 }
 
-class CanvasComponent extends React.Component {
+class BGCanvasComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -39,9 +40,7 @@ class CanvasComponent extends React.Component {
 
   render() {
     return (
-      <div id="canvas-holder" className="row justify-content-center">
-        <canvas ref="canvas" id="profile-picture" width="400" height="400"></canvas>
-      </div>
+      <canvas ref="canvas" id="canvas-bg" width="400" height="400"></canvas>
     );
   }
 }
@@ -106,6 +105,42 @@ class RGBController extends React.Component {
   }
 }
 
+class ImageCanvasComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rgb: [60, 155, 236],
+      rotation: 0
+    }
+  }
+
+  componentDidMount() {
+    this.updateCanvas();
+  }
+
+  componentDidUpdate() {
+    this.updateCanvas();
+  }
+
+  updateCanvas() {
+    const context = this.refs.canvas.getContext('2d');
+    var dp_img = document.createElement('img');
+    dp_img.onload = function() {
+      context.drawImage(dp_img,
+                        // Note that the canvas width and height is 400.
+                        200 - dp_img.width / 2,
+                        200 - dp_img.height / 2);
+    }
+    dp_img.src = 'assets/images/dp.png';
+  }
+
+  render() {
+    return (
+      <canvas ref="canvas" id="canvas-picture" width="400" height="400"></canvas>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
@@ -115,7 +150,10 @@ class App extends Component {
           <h2 className="thin-font">by Sean Ballais (<a href="https://twitter.com/seanballais">@seanballais</a>)</h2>
         </header>
         <article className="container">
-          {<CanvasComponent />}
+          <div id="canvas-holder" className="row justify-content-center">
+            {<BGCanvasComponent />}
+            {<ImageCanvasComponent />}
+          </div>
           {<RGBController />}
         </article>
       </div>
