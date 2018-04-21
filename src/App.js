e046ba89f7e83d20ae103a704bb94c0b6cf738ec
Sean Francis N. Ballais <sean@seanballais.com>
2018-04-22 00:58:05 +0800

    Completed background changing and image rotation.

--- src/App.js
+++ src/App.js
@@ -77,41 +77,56 @@ class RGBController extends React.Component {
   render() {
     return (
       <div id="sliders" className="row justify-content-center">
-        <div className="col-4">
-          Red | {this.state.rgb[0]}
-          <Slider
-            min={0}
-            max={255}
-            value={this.state.rgb[0]}
-            onChange={this.handleChangeOnRed}
-          />
-          Green | {this.state.rgb[1]}
-          <Slider
-            min={0}
-            max={255}
-            value={this.state.rgb[1]}
-            onChange={this.handleChangeOnGreen}
-          />
-          Blue | {this.state.rgb[2]}
-          <Slider
-            min={0}
-            max={255}
-            value={this.state.rgb[2]}
-            onChange={this.handleChangeOnBlue}
-          />
+        <div className="col-6">
+          <div id="red-slider">
+            Red | {this.state.rgb[0]}
+            <Slider
+              min={0}
+              max={255}
+              value={this.state.rgb[0]}
+              onChange={this.handleChangeOnRed}
+            />
+          </div>
+          <div id="green-slider">
+            Green | {this.state.rgb[1]}
+            <Slider
+              min={0}
+              max={255}
+              value={this.state.rgb[1]}
+              onChange={this.handleChangeOnGreen}
+            />
+          </div>
+          <div id="blue-slider">
+            Blue | {this.state.rgb[2]}
+            <Slider
+              min={0}
+              max={255}
+              value={this.state.rgb[2]}
+              onChange={this.handleChangeOnBlue}
+            />
+          </div>
         </div>
       </div>
     );
   }
 }
 
+function updateCanvasRotation(degrees) {
+  this.setState({
+    rgb: this.state.rgb,
+    rotation: degrees
+  });
+}
+
 class ImageCanvasComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      rgb: [60, 155, 236],
+      rgb: [],
       rotation: 0
     }
+
+    updateCanvasRotation = updateCanvasRotation.bind(this);
   }
 
   componentDidMount() {
@@ -123,20 +138,67 @@ class ImageCanvasComponent extends React.Component {
   }
 
   updateCanvas() {
-    const context = this.refs.canvas.getContext('2d');
+    var context = this.refs.canvas.getContext('2d');
     var dp_img = document.createElement('img');
+    var imageCanvasState = this.state;
     dp_img.onload = function() {
-      context.drawImage(dp_img,
-                        // Note that the canvas width and height is 400.
-                        200 - dp_img.width / 2,
-                        200 - dp_img.height / 2);
+      var imageCache = this;
+      context.save();
+      context.clearRect(0, 0, 400, 400);
+      context.translate(imageCache.width / 2, imageCache.height / 2);
+      context.rotate(Math.PI / 180 * imageCanvasState.rotation);
+
+      context.drawImage(
+        dp_img, 
+        // Note that the canvas width and height is 400.
+        -imageCache.width / 2,
+        -imageCache.height / 2,
+        imageCache.width,
+        imageCache.height
+      );
+      context.restore();
     }
+
     dp_img.src = 'assets/images/dp.png';
   }
 
   render() {
     return (
       <canvas ref="canvas" id="canvas-picture" width="400" height="400"></canvas>
+    );
+  }
+}
+
+class ImageController extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      rotation: 0
+    }
+  }
+
+  handleChangeOnDegrees = (value) => {
+    this.setState({
+      rotation: value
+    });
+    updateCanvasRotation(value);
+  }
+
+  render() {
+    return (
+      <div id="sliders" className="row justify-content-center">
+        <div className="col-6">
+          <div id="rotation-slider">
+            Rotation | {this.state.rotation}&#176;
+            <Slider
+              min={0}
+              max={360}
+              value={this.state.rotation}
+              onChange={this.handleChangeOnDegrees}
+            />
+          </div>
+        </div>
+      </div>
     );
   }
 }
@@ -154,7 +216,10 @@ class App extends Component {
             {<BGCanvasComponent />}
             {<ImageCanvasComponent />}
           </div>
-          {<RGBController />}
+          <div id="canvas-sliders" className="row justify-content-center">
+            {<RGBController />}
+            {<ImageController />}
+          </div>
         </article>
       </div>
     );
